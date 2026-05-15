330842593b4ee0c88419eb0f19bdf4894370520a
Phani Madhuri Vadlamani <phanimadhuri006@gmail.com>
2026-05-16 00:04:25 +0530

    Added db error logging

--- ProBackend/server.js
+++ ProBackend/server.js
@@ -35,8 +35,9 @@ const connectDB = async () => {
     const port = process.env.PORT || 5000;
     app.listen(port, () => console.log(`server listening on ${port}..`));
   } catch (err) {
-    console.log("err in db connect", err);
-  }
+  console.error("DB CONNECTION ERROR:");
+  console.error(err);
+}
 };
 
 connectDB();
