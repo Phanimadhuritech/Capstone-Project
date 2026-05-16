2945983a3d1f80bea293ba9a8caf51cec2e44d4d
Phani Madhuri Vadlamani <phanimadhuri006@gmail.com>
2026-05-16 16:29:20 +0530

    Added detailed db logd

--- ProBackend/server.js
+++ ProBackend/server.js
@@ -29,6 +29,7 @@ app.use("/auth", commonApp);
 //connect to db
 const connectDB = async () => {
   try {
+    console.log("DB URL:", process.env.DB_URL);
     await connect(process.env.DB_URL);
     console.log("DB server connected");
     //assign port
